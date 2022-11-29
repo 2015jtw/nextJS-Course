ed233f67ec567aa5039129a88feb21a33f773840
Josh <2015jtw@gmail.com>
2022-11-29 16:58:40 -0500

    finished intro

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,27 @@
 import styles from '../../styles/Home.module.css'
 
 export default function ProductPage() {
+
+    const products = [
+        {
+            id: 1,
+            name: 'Broom'
+        },
+        {
+            id: 2,
+            name: 'Razor'
+        },
+        {
+            id: 3,
+            name: 'Tooth brush'
+        }
+    ]
     return (
         <div className={styles.container}>
             <h1>Main Product Page</h1>
+            <ul>
+                {products.map(p => <li key={p.id}>{p.name}</li>)}
+            </ul>
         </div>
     )
 }
